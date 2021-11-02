b8354d8ad066afcf0b3e2aeb2620fa33b4a20490
Alonso <joalmr@gmail.com>
2021-11-02 15:54:44 -0500

    prueba imagenes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,7 +8,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
       home: HomePage(),
       //
     );
